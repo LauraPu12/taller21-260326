ba0b444ad162b755d5993958858d3a670642e4cf
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-03-26 10:38:51 -0500

    Se crea la clase ListaEnlazada creando primero el metodo de agregar el dato entero y el segundo metodo es mostrar el dato

---
+++ taller21-260326/src/taller21/pkg260326/ListaEnlazada.java
@@ -0,0 +1,37 @@
+package taller21.pkg260326;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author anair
+ */
+public class ListaEnlazada {
+    Nodo cabeza;//es el primer nodo de la lista
+
+   public void agregar(int dato){//metodo para agregar o insertar
+       Nodo nuevo = new Nodo(dato);
+       
+       if (cabeza==null){//si la lista esta vacia
+           cabeza=nuevo;
+       }else{
+           Nodo aux=cabeza; //nodo auxiliar para recorrer
+           
+           while(aux.siguiente!=null){//para el siguiente 
+               aux=aux.siguiente;
+           }
+           aux.siguiente = nuevo;//para insertar al final
+       }
+   }
+   public void mostrar(){//se crea el metodo para mostarlo
+       Nodo aux =cabeza; //para que incie desde la cabeza
+       
+       while(aux!=null){ //con esto se recorre la lista hasta el final
+           System.out.print(aux.dato + " ");//se imprime el dato
+           aux = aux.siguiente;
+       }
+   }
+}
